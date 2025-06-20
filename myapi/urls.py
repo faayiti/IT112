5aa8989b9e0bb3b995f844c4f4c5e5670c2beebc
Safia <safia@fakeemail.com>
2025-06-19 21:26:37 -0700

    Move project files into assignment folder

--- myapi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),             # <-- This line added for homepage
-    path('items/', views.all_items),
-    path('item/', views.single_item),
-    path('add-item/', views.create_item),
-]
