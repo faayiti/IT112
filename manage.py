a69f92d7a9b782d7fe5317496ffd2d9e1caf3562
Safia <safia@fakeemail.com>
2025-06-19 21:14:42 -0700

    Complete Django REST API assignment

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beauty_project.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
->>>>>>> 0942db4755452007bf2966e1da136f3710117053
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastproject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
